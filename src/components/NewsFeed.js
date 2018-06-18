3475e9fd58b3ae059c850da61c2783844ef593f5
MayuriR29 <mayuri.gr@gmail.com>
2018-06-18 16:30:46 +0800

    changes for vote

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,18 +1,45 @@
 import React, { Component } from "react";
-
+import { articles } from "../seedData";
 class NewsFeed extends Component {
   constructor() {
     super();
     this.state = {
-      articles: []
+      key: articles
     };
   }
-
+  
   render() {
-    return this.state.articles.map((article, i) => {
+    
+    return this.state.key.map((eachArticle,i) => {
       // TODO: start coding!
-      return <li key={i}></li>;
+      return (
+        <div key={i} id="eachArticle">
+          <h1>{eachArticle.title}</h1>
+          <h2>{eachArticle.author}</h2>
+          <p>{eachArticle.description}</p>
+          <a href={eachArticle.url}><img src={eachArticle.urlToImage} alt="Rob Roy Glacier"/> </a>
+          
+          <p>Votes: {eachArticle.votes}</p> 
+          <button onClick={() => this.handleClick(eachArticle)}>+</button>
+        </div>
+      );
     });
+  }
+  handleClick(eachArticle) {   
+    const index = this.state.key.indexOf(eachArticle);
+    const count= eachArticle.votes+1;  
+    const updatedArticle={
+      ...eachArticle,
+      votes: count
+    }
+    console.log('updatedArticle',updatedArticle);
+    this.setState({
+      key:[
+        ...this.state.key.slice(0,index),
+        updatedArticle,
+        ...this.state.key.slice(index+1)
+      ]
+    })
   }
 }
 
